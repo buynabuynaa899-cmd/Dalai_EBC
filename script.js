2adef9ce7303d63b23e91b106add743c44760c4a
buynabuynaa899-cmd <buynabuynaa899@gmail.com>
2026-02-09 15:01:59 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
+<script src="https://www.gstatic.com/firebasejs/10.8.0/firebase-app-compat.js"></script>
+    <script src="https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore-compat.js"></script>
+
+    <script src="script.js"></script>
+</body>
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCkSj1k1rEislrK0SmTrUWDDd75d2QgbLE",
+  authDomain: "dalai-ebc.firebaseapp.com",
+  projectId: "dalai-ebc",
+  storageBucket: "dalai-ebc.firebasestorage.app",
+  messagingSenderId: "872196747374",
+  appId: "1:872196747374:web:6c7e2c5558e1a9eca6367f",
+  measurementId: "G-8EVRYZEQYC"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 // Firebase-ийн функцүүдийг ашиглан бүртгэл нэмэх
 async function registerUser(event) {
     event.preventDefault();
